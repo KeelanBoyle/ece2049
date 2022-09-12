112c1ce140604170ce530d2eaf6e20592b6ffae9
Keelan Boyle <kboyle@wpi.edu>
2022-09-12 17:46:04 -0400

    ui stuff

--- workspace/Lab1/main.c
+++ workspace/Lab1/main.c
@@ -5,21 +5,38 @@
 #include <gameloop.h>
 #include "peripherals.h"
 
-State g_State = GAME_LOOP;
+State g_State = MAIN_MENU;
 
 void loop(void) {
+
+
+    char     intro[14]="SPACE INVADERS";
+    char     defeat[20]="RESISTANCE IS FUTILE";
     switch(g_State) {
     case MAIN_MENU:
+
+        displayMessage(intro);
+        unsigned char  currKey = getKey();
+        if(currKey== '*'){
+            countDown();
+            g_State = GAME_LOOP;
+        }
         break;
     case GAME_LOOP:
     	gameLoop();
         break;
     case GAME_OVER:
+
+      displayMessage(defeat);
+      //wait some time
+      g_State = MAIN_MENU;
         break;
     default:
+
         g_State = GAME_OVER;
         break;
     }
+    Graphics_flushBuffer(&g_sContext);
 }
 
 
@@ -27,7 +44,7 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	configDisplay();
-	
+	configKeypad();
 	initGameState();
 
 	while(1) {
