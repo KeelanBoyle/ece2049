66fef9eadf83aa1a5cff7d101d1951dce15ebbdb
Arthur Ames <arthurames9@gmail.com>
2022-09-13 15:50:50 -0400

    update gitignore

--- workspace/Lab1/welcome.c
+++ workspace/Lab1/welcome.c
@@ -5,7 +5,7 @@
  *      Author: kboyl
  */
 #include <msp430.h>
-#include "peripherals.h"
+#include "src/peripherals.h"
 
 void displayMessage(char message[]){
     Graphics_clearDisplay(&g_sContext);
@@ -19,9 +19,9 @@ void displayMessage(char message[]){
 void countDown(){
     Graphics_clearDisplay(&g_sContext);
     Graphics_drawStringCentered(&g_sContext, "3...", AUTO_STRING_LENGTH, 48, 15, TRANSPARENT_TEXT);
-//wait
+    //wait
     Graphics_drawStringCentered(&g_sContext, "2...", AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
-//wait
+    //wait
     Graphics_drawStringCentered(&g_sContext, "1...", AUTO_STRING_LENGTH, 48, 55, TRANSPARENT_TEXT);
     //wait
 }
