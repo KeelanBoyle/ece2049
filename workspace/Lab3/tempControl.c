6f603889ca9d793d4ce4d710a4585a5d277a731f
Keelan Boyle <kboyle@wpi.edu>
2022-10-05 23:07:21 -0400

    adc help

--- workspace/Lab3/tempControl.c
+++ workspace/Lab3/tempControl.c
@@ -7,9 +7,9 @@
 void adcSetup(){
     REFCTL0 &= ~REFMSTR;
     ADC12CTL0 = ADC12SHT0_10 + ADC12SHT1_10 + ADC12REFON + ADC12ON + ADC12MSC;
-    ADC12CTL1 = ADC12CSTARTADD_7 + ADC12SHP;
+    ADC12CTL1 = ADC12CSTARTADD_7 + BIT8;
     P6SEL |= BIT6;
-    ADC12MCTL7 = ADC12SREF_0 + ADC12INCH_6;  // this is for Full scale range
+    ADC12MCTL7 = ADC12SREF_0 + ADC12INCH_10;  // this is for Full scale range
     ADC12MCTL8 = ADC12SREF_2 + ADC12INCH_13;
     ADC12MCTL9 = ADC12SREF_1 + ADC12INCH_10 + ADC12EOS; // input channel 10, the referenced used is Ref_1, V(R+) = VREF+ = 1.5 V, and V(R-) = Vss. The full-scale range is 1.5 V
     ADC12CTL0 |= (ADC12SC|ADC12ENC);
@@ -24,13 +24,12 @@ int* getADCValue(){
     while (ADC12CTL1 & ADC12BUSY){
      __no_operation();
 
-    value1 = ADC12MEM7 & 0x0FFF; // get low 12 bits output memory register 7
-    value2 = ADC12MEM8 & 0x0FFF; // get low 12 bits output memory register 8
-    value3 = ADC12MEM9 & 0x0FFF; // get low 12 bits output memory register 9
+    value1 = ADC12MEM9 & 0x0FFF; // get low 12 bits output memory register 7
+    value2=ADC12MEM9;
 
     }
-    int adcData[3]={value1,value2,value3};
-    return 1000;
+
+    return value1;
 }
 
 double averageTempC(){
@@ -54,7 +53,7 @@ double averageTempC(){
         double averageADC=totalTemp/elements;
         //convert from adc to temprature
        // double averageTemp= (averageADC-3.3)*(16.6666)+30;//???
-        double VrefP=3.3;
+        double VrefP=1.5;
         double VrefN=0;
         double bitCount=pow(2,10);
         double averageTemp= averageADC*(VrefP-VrefN)/bitCount+VrefP;
