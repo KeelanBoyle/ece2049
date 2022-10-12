911e1b42e2ed1b7bd975b106cbfbf8bfb2f48156
Arthur Ames <arthurames9@gmail.com>
2022-10-12 16:57:18 -0400

    wave generator final

--- workspace/Lab4/wavegenerator.c
+++ workspace/Lab4/wavegenerator.c
@@ -14,7 +14,7 @@
 #define TMR_FREQ  (32768 * 32)
 #define TMR_CLR   (TA2CTL |= TACLR)
 
-#define RESOLUTION 1024
+#define RESOLUTION 32
 #define DAC_MAX 4096
 
 #define STEP (DAC_MAX / RESOLUTION)
@@ -23,6 +23,8 @@ uint32_t freq = 0;
 WaveSetting waveMode = DC;
 uint16_t dcLevel = 0;
 uint16_t acLevel = 0;
+uint8_t triangleStatus = 1;
+uint16_t triangleCounter = 0;
 
 bool setFreq(uint32_t hz) {
     if(hz < 100 || hz > 1000) {
@@ -40,6 +42,10 @@ void setWaveMode(WaveSetting setting) {
     if(waveMode == DC) {
         TMR_PAUSE;
         DACSetValue(dcLevel);
+    } else if(waveMode == TRIANGLE) {
+        triangleStatus = 1;
+        triangleCounter = 0;
+        TMR_PLAY;
     }
     else {
         TMR_PLAY;
@@ -86,11 +92,27 @@ __interrupt void timer_A2_ISR(void)
         return;
     case TRIANGLE:
         TMR_PAUSE;
-        TA2CCR0 = (uint16_t)(TMR_FREQ / (freq * RESOLUTION));
+        volatile uint16_t ccr0 = (TMR_FREQ / (freq * RESOLUTION));
+        TA2CCR0 = ccr0;
         TMR_CLR;
         TMR_PLAY;
 
-        acLevel = ~acLevel;
+        if(triangleStatus == 1) {
+            triangleCounter++;
+            acLevel = (acLevel + STEP) % DAC_MAX;
+            if(triangleCounter == RESOLUTION) {
+                triangleStatus = 0;
+            }
+        }
+
+        if(triangleStatus == 0) {
+            triangleCounter--;
+            acLevel = (acLevel - STEP);
+            if(triangleCounter == 0) {
+                triangleStatus = 1;
+            }
+        }
+
         DACSetValue(acLevel & 0xFFF);
         return;
     default:
